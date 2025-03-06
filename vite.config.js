7bdec386b5cce554b3eb826c302a20e93a7b65f1
Yuan Gao <ygao@my.wctc.edu>
2025-03-05 19:41:28 -0600

    ainiyaa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',
-  base: '/aini/',
+// https://vite.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+  }
+  
+  if (command === 'build') {
+    config.base = '/aini/' // 仅在构建时使用此路径
+  }
+  
+  return config
 })
